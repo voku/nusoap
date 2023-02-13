c482f615404758de5fcc12b4ceb7447011c10a60
s-renier-taonix-fr <s.renier@taonix.fr>
2023-02-13 11:18:57 +0800

    Fix : Wrong type for '+' and '-' operators

--- src/nusoap.php
+++ src/nusoap.php
@@ -992,11 +992,11 @@ function iso8601_to_timestamp($datestr)
             $h = substr($regs[8], 1, 2);
             $m = substr($regs[8], strlen($regs[8]) - 2, 2);
             if ($op == '-') {
-                $regs[4] = $regs[4] + $h;
-                $regs[5] = $regs[5] + $m;
+                $regs[4] = intval ($regs[4]) + intval ($h);
+                $regs[5] = intval ($regs[5]) + intval ($m);
             } elseif ($op == '+') {
-                $regs[4] = $regs[4] - $h;
-                $regs[5] = $regs[5] - $m;
+                $regs[4] = intval ($regs[4]) - intval ($h);
+                $regs[5] = intval ($regs[5]) - intval ($m);
             }
         }
         return gmmktime($regs[4], $regs[5], $regs[6], $regs[2], $regs[3], $regs[1]);
